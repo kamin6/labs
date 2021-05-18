9ce6ef62c7563e2197da586660fd82effbcca0f0
Mark Botha <bothamar@amazon.com>
2021-05-18 13:57:32 +0100

    Initial BankAccount

---
+++ BankAccount.java
@@ -0,0 +1,49 @@
+public class BankAccount {
+    protected String Customer;
+    protected int AccountNumber;
+    protected float Balance;
+
+    protected int currentAccountNumber = 100000;
+
+    public float getBalance() {
+        return this.Balance;
+    }
+
+    public void setBalance(float Balance) {
+        this.Balance = Balance;
+    }
+
+    public int getAccountNumber() {
+        return this.AccountNumber;
+    }
+
+    public void setAccountNumber(int AccountNumber) {
+
+        if (AccountNumber == null){
+            this.AccountNumber = currentAccountNumber++;
+        }
+
+        this.AccountNumber = AccountNumber;
+    }
+
+
+    public String getCustomer() {
+        return this.Customer;
+    }
+
+    public void setCustomer(String Customer) {
+        this.Customer = Customer;
+    }
+
+    public void Deposit(int Amount) {
+        this.Balance += Amount;
+    }
+
+    public void Withdraw(int Amount) {
+        this.Balance -= Amount;
+    }
+
+
+
+
+}
